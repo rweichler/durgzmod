3049ae8b7fd7471fb2ac9540e0bba070fd295825
Reed Weichler <rweichler@gmail.com>
2013-09-26 18:50:23 -0700

    FINALLY fixed that fucking alcohol error

--- lua/entities/durgz_alcohol/init.lua
+++ lua/entities/durgz_alcohol/init.lua
@@ -9,7 +9,7 @@ ENT.LASTINGEFFECT = 45; --how long the high lasts in seconds
 
 
 local function shouldnt_do_that_shit(pl)
-    return not pl or pl == nil or not pl:GetActiveWeapon() or pl:GetNetworkedFloat("durgz_alcohol_high_end") < CurTime()
+    return pl == NULL or not pl or pl == nil or not pl:GetActiveWeapon() or pl:GetNetworkedFloat("durgz_alcohol_high_end") < CurTime()
 end
 
 function ENT:High(activator,caller)
@@ -23,7 +23,7 @@ function ENT:High(activator,caller)
 	
 	for i = 1,thing do
 		timer.Simple(math.Rand(5,10), function()
-			if( activator && activator:GetNetworkedFloat("durgz_alcohol_high_end") - TRANSITION_TIME > CurTime() )then
+			if activator and activator:GetNetworkedFloat("durgz_alcohol_high_end") - TRANSITION_TIME > CurTime() then
 				local cmd = commands[math.random(1, #commands)]
 				activator:ConCommand("+"..cmd)
 				timer.Simple(1, function()
@@ -34,20 +34,21 @@ function ENT:High(activator,caller)
 	end
 	
 	--takes out the pistol and then shoots randomly
-	local oldwep = activator:GetActiveWeapon()
+	local oldwep = activator:GetActiveWeapon():GetClass()
 	
 
 
-	if( !oldwep )then return; end
+	if not oldwep then return end
 	for id,wep in pairs(activator:GetWeapons())do
 		if( wep:GetClass() == "weapon_pistol" )then
 			activator:SelectWeapon("weapon_pistol")
 			timer.Simple(0.3, function() if shouldnt_do_that_shit(activator) then return end
 				activator:ConCommand("+attack")
-				timer.Simple(0.1, function() if shouldnt_do_that_shit(activator) then return end
+				timer.Simple(0.1, function() if activator == NULL or not activator or activator == nil then return end
 					activator:ConCommand("-attack")
-					if(oldwep == NULL || !oldwep || !activator:Alive())then return end
-                        activator:SelectWeapon(oldwep:GetClass())
+					if oldwep and oldwep != nil and oldwep != NULL and activator:Alive() then
+                        activator:SelectWeapon(oldwep)
+                    end
 				end)
 			end)
 		end
